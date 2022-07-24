b3bbaa397930b56d125a7f44818ad602e5a9ed9c
Tonye Roberts <talentedrex@gmail.com>
2022-07-24 23:20:35 +0100

    update deployment files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a00bf57605cb64ab5aa533fed7aa133e-1620211535.eu-west-3.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
